b173dc311d349b010d2653ced8734a683aaf5601
Escalante <Angel.Escalante@kantar.com>
2022-07-26 15:18:47 -0500

    Añade archivos finales

--- mvtFamiliares/mvtFamiliares/urls.py
+++ mvtFamiliares/mvtFamiliares/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from relatives.views import add_relatives, list_relatives
 from mvtFamiliares.views import hello_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_page)
+    path('hello/', hello_page, name="hello"),
+    path('add_relatives/', add_relatives, name="add_relatives"),
+    path('list_relatives/', list_relatives, name="list_relatives")
 ]
--- mvtFamiliares/relatives/views.py
+++ mvtFamiliares/relatives/views.py
@@ -1,3 +1,22 @@
+from multiprocessing import context
 from django.shortcuts import render
+from relatives.models import Relatives
 
-# Create your views here.
+# Añade familiares a la database
+def add_relatives(request):
+    new_relative = Relatives.objects.create(name = 'Tomás', age = 3)
+    new_relative_2 = Relatives.objects.create(name = 'Lola', age = 7, nickname = "Gorda")
+    new_relative_3 = Relatives.objects.create(name = 'Alberto', age = 15)
+    context = {
+        'new_relative' : new_relative,
+        'new_relative_2' : new_relative_2,
+        'new_relative_3' : new_relative_3
+    }
+    return render(request, 'add_relative.html', context=context)
+
+def list_relatives(request):
+    relatives = Relatives.objects.all()
+    context = {
+        'relatives' : relatives
+    }
+    return render(request, 'see_relatives.html', context=context)
